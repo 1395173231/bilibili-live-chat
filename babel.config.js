08920a7f8641144a945251420ddbf18ad1f535e1
1395173231 <35016247+1395173231@users.noreply.github.com>
2023-04-06 07:36:42 +0000

    Deploying to dist from @ 1395173231/bilibili-live-chat@e524141b7cbd8eaf5374f796d916ba3152c0d8dc 🚀

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  plugins: ['lodash', '@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'],
-  presets: ['@vue/cli-plugin-babel/preset'],
-};
--- src/pages/live/main.js
+++
@@ -1,5 +0,0 @@
-import '@/utils/bufferPolyfill';
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
